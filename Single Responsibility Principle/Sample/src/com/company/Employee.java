bd9fb6736e9d4ce5d97a73d72f3611f81ddb9784
Hady Salhab <hadysalhab1@gmail.com>
2020-01-22 17:01:32 +1100

    Single Responsibility Principle VIOLATION sample

---
+++ Single Responsibility Principle/Sample/src/com/company/Employee.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Date;
+
+public class Employee {
+    private String employeeId;
+    private String name;
+    private String address;
+    private Date dateOfJoining;
+
+    public boolean isPromotionDueThisYear(){
+
+    }
+
+    public double calcIncomeTaxForCurrentYear(){
+
+    }
+}
