d0482f173cc0943b04b73fd618352550501f52f0
Hady Salhab <hadysalhab1@gmail.com>
2020-01-22 17:35:35 +1100

    Open Closed Princimple FIXED

--- Open Closed Principle/Sample/src/com/company/Rectanle.java
+++ Open Closed Principle/Sample/src/com/company/Rectanle.java
@@ -1,25 +1,28 @@
 package com.company;
 
-public class Rectanle {
+public class Rectanle implements Shape {
     public double length;
     public double width;
+
+    @Override
+    public double calculateArea() {
+        return length*width;
+    }
 }
 
-class Circle {
+class Circle implements Shape {
     public double radius;
+
+    @Override
+    public double calculateArea() {
+        return (22/7)*radius*radius;
+    }
 }
 
 class AreaCalculator {
-    //IN this case the AreaCalculator class is OPEN to modification, with every shape added, we need to
-    //add new method so we can calculate the area.
-    public double caculateRectangleArea(Rectanle rectangle) {
-        return rectangle.length * rectangle
-                .width;
-
+    //IN this case the AreaCalculator class is CLOSED to modification, with every shape added, we don't need to
+    //add new method to calculate the area.
+    public double calculateShapeArea(Shape shape) {
+        return shape.calculateArea();
     }
-
-    public double calculateCircleArea(Circle circle) {
-        return (22 / 7) * circle.radius * circle.radius;
-    }
-
 }
