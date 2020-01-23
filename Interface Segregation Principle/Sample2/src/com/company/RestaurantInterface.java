4308d31c91316dc876d1180b19b75db66852d464
Hady Salhab <hadysalhab1@gmail.com>
2020-01-23 18:18:27 +1100

    Adding Comment Explaining why Violation Exist in sample 2

--- Interface Segregation Principle/Sample2/src/com/company/RestaurantInterface.java
+++ Interface Segregation Principle/Sample2/src/com/company/RestaurantInterface.java
@@ -2,6 +2,8 @@ package com.company;
 
 public interface RestaurantInterface {
     //violation
+    //clients who order online has to override unnecessary methods (like payInPerson)
+    //clients who order inPerson has to override methods related to online process
     void acceptOnlineOrder();
     void takeTelephoneOrder();
     void payOnline();
