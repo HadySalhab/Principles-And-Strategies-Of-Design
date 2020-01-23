c2c33d9ede6a45b044517aa1576c49fe862f83df
Hady Salhab <hadysalhab1@gmail.com>
2020-01-23 20:34:07 +1100

    DInversion principle fixed sample

--- Dependency Inversion Principle/Sample/src/com/company/PasswordReminder.java
+++ Dependency Inversion Principle/Sample/src/com/company/PasswordReminder.java
@@ -1,10 +1,20 @@
 package com.company;
 
+interface DBConnectionInterface {
+    public int connect();
+}
+class MySqlConnection implements DBConnectionInterface{
+    @Override
+    public int connect() {
+        return 1;
+    }
+}
 
 public class PasswordReminder {
-    private int dbConnection;
-    //Violation of the DI principle because, we are relying on concrete class.(MySqlConnection)
-    public  PasswordReminder(MySqlConnection dbConnection){
+    private DBConnectionInterface dbConnection;
+
+    //no more dependency on a low level class, we are relying on the high level class of connection (DBConnection)
+    public PasswordReminder(DBConnectionInterface dbConnection) {
         this.dbConnection = dbConnection;
     }
 }
