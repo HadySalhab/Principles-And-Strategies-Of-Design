ed6d2a1793873da8ae144bc655e1aa4b2ae16811
Hady Salhab <hadysalhab1@gmail.com>
2020-01-23 18:16:44 +1100

    Violation of ISP example2

---
+++ Interface Segregation Principle/Sample2/src/com/company/OnlineClientImpl.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import com.company.RestaurantInterface;
+
+public  class OnlineClientImpl implements RestaurantInterface {
+
+    @Override
+    public void acceptOnlineOrder() {
+
+    }
+
+    @Override
+    public void takeTelephoneOrder() {
+
+    }
+
+    @Override
+    public void payOnline() {
+
+    }
+
+    @Override
+    public void walkInCustomerOrder() {
+
+    }
+
+    @Override
+    public void payInPerson() {
+
+    }
+}
---
+++ Interface Segregation Principle/Sample2/src/com/company/RestaurantInterface.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public interface RestaurantInterface {
+    //violation
+    void acceptOnlineOrder();
+    void takeTelephoneOrder();
+    void payOnline();
+    void walkInCustomerOrder();
+    void payInPerson();
+}
+
+
